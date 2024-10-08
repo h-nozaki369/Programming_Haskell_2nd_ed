862d6c8609768fd5333b7f73cac56e0ca3d25bf5
Hajime Nozaki <nozaki369@outlook.jp>
2024-10-08 17:54:22 +0900

    update ch14ex4.hs

--- ch14ex4.hs
+++ ch14ex4.hs
@@ -1,10 +1,24 @@
+import Data.Foldable
+
 data Tree a = Leaf | Node (Tree a) a (Tree a)
               deriving Show
 
-instance Foldable Tree a where
+instance Functor Tree where
+    -- fmap :: (a -> b) -> Tree a -> Tree b
+    fmap g Leaf       = Leaf
+    fmap g (Node l x r) = Node (fmap g l) (g x) (fmap g r)
+
+instance Applicative Tree where
+    -- pure :: a -> Tree a
+    pure x = Node Leaf x Leaf
+    
+    -- (<*>) :: Tree (a -> b) -> Tree a -> Tree b
+    Node _ g _ <*> Node l x r = Node Leaf (g x) Leaf
+ 
+instance Foldable Tree where
     -- fold :: Monoid a => Tree a -> a
     fold Leaf         = mempty
-    fold (Node l x r) = fold l `mappend` x `mappend` fold r
+    fold (Node l x r) = (fold l) `mappend` x `mappend` (fold r)
 
     -- foldMap :: Monoid b => (a -> b) -> Tree a -> b
     foldMap _ Leaf         = mempty
@@ -12,8 +26,13 @@ instance Foldable Tree a where
 
     -- foldr :: (a -> b -> b) -> b -> Tree a -> b
     foldr g y Leaf         = y
-    foldr g y (Node l x r) = foldr g y l `mappend` g x `mappend` foldr g y r
+    foldr g y (Node l x r) = foldr g (g x (foldr g y r)) l
 
     -- foldl :: (a -> b -> a) -> a -> Tree b -> a
     foldl g x Leaf         = x
-    foldl g x (Node l y r) = foldl g x l `mappend` g y `mappend` foldl g x r
+    foldl g x (Node l y r) = foldl g (g (foldl g x l) y) r
+
+instance Traversable Tree where
+    -- traverse :: Applicative f => (a -> f b) -> Tree a -> f (Tree b)
+    traverse g Leaf         = pure Leaf
+    traverse g (Node l x r) = Node <$> traverse g l <*> g x <*> traverse g r
